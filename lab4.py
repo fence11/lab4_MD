e3b18df922801cd7d2523772be5e068fb753bb69
fence11 <125548004+fence11@users.noreply.github.com>
2024-04-24 01:50:47 +0300

    Add files via upload

--- lab4.py
+++ lab4.py
@@ -11,68 +11,6 @@ def create_adjacency_matrix(num_nodes, nodes_input):
     return graph
 
 
-def create_incidence_matrix(nodes_input):
-    graph = []
-    for i, connections_with_weights in enumerate(nodes_input):
-        for conn, _ in connections_with_weights:
-            conn_node = int(conn.strip()) - 1
-            graph.append((i + 1, conn_node + 1))
-    return graph
-
-
-def create_incidence_list(num_nodes, nodes_input):
-    graph = [[0] * num_nodes for _ in range(num_nodes)]
-    for i, connections_with_weights in enumerate(nodes_input):
-        for conn, _ in connections_with_weights:
-            conn_node = int(conn.strip()) - 1
-            graph[i][conn_node] = 1
-    return graph
-
-
-def print_adjacency_matrix(graph):
-    print("   | ", end='')
-    for i in range(len(graph)):
-        print(f" x{i+1} |", end='')
-    print()
-
-    for i, row in enumerate(graph):
-        print(f"x{i+1} | ", end='')
-        for val in row:
-            print(f" {val}  |", end='')
-        print()
-
-
-def print_incidence_matrix(graph, num_nodes):
-    print("       | ", end='')
-    for i in range(num_nodes):
-        print(f" x{i+1} |", end='')
-    print()
-
-    for pair in graph:
-        print(f"{pair} | ", end='')
-        for i in range(1, num_nodes + 1):
-            if i == pair[0] == pair[1]:
-                print("  2 |", end='')
-            elif i == pair[0]:
-                print(" -1 |", end='')
-            elif i == pair[1]:
-                print("  1 |", end='')
-            else:
-                print("  0 |", end='')
-        print()
-
-
-def print_incidence_list(graph):
-    for i, row in enumerate(graph):
-        connections = []
-        for x, val in enumerate(row):
-            # print(f"{x} && {val}") # col_header - 1 && row value
-            if val == 1:
-                connections.append(str(x + 1))
-        connections_str = ' '.join(connections)
-        print(f"{i + 1} | {connections_str} 0")
-
-
 def weighted_graph_connections(num_nodes):
     nodes_input = []
     print("node paths:")
@@ -154,17 +92,17 @@ def calc_bellman_kalaba(matrix):
         iteration += 1
 
 
-def print_bellman_kalaba(matrix):
-    num_nodes = len(matrix[0]) - 1  # exclude v(0)
+def print_bellman_kalaba(matrix, nodes_input):
+    num_nodes = len(matrix)  # Get the number of nodes from the matrix size
 
-    # print default square matrix
+    # Print default square matrix
     for i, row in enumerate(matrix):
         print(f"{i + 1:2} ", end="")
         formatted_row = [f" {elem:2}" if elem != '+' and elem !=
                          float('inf') else " + " for elem in row]
         print("[" + ",".join(formatted_row) + "]")
 
-    # print rows V0, V1, V2 . . .
+    # Print rows V0, V1, V2 . . .
     iteration = 1
     equality_found = False
     while not equality_found:
@@ -177,31 +115,111 @@ def print_bellman_kalaba(matrix):
         iteration += 1
     print("Equality found. Proceeding...")
 
+    # Find the shortest path using v_values
+    current_index = 0
+    searched_index = 1
+    shortest_path = []
+    for i in range(num_nodes):  # Loop through each node
+        if matrix[current_index][searched_index].isdigit() and v_values[iteration-1][current_index] - int(matrix[current_index][searched_index]) == v_values[iteration-1][searched_index]:
+            print(f"{v_values[iteration-1][current_index]} - {matrix[current_index][searched_index]} = {v_values[iteration-1][searched_index]}")
+            shortest_path.append(current_index + 1)  # Append the current node to the shortest path
+            if matrix[current_index][searched_index].isdigit() and v_values[iteration-1][current_index] - int(matrix[current_index][searched_index]) == 0:
+                shortest_path.append(searched_index + 1)  # Append the searched node to the shortest path
+                break
+            current_index = searched_index  # Move to the searched node
+            searched_index += 1  # Increment the searched index
+        else:
+            searched_index += 1
 
-def menu(num_nodes, nodes_input):
+    print(f"Shortest path: {shortest_path}")
+
+    # Print node coordinates and their corresponding weights
+    print("\nNode coordinates - Weight pairs:")
+    for i, connections_with_weights in enumerate(nodes_input):
+        for conn, weight in connections_with_weights:
+            print(f"Node {i+1} -> Node {conn}: Weight {weight}")
+
+
+def ford_shortest_path(num_nodes, nodes_input, source_node):
+    distances = [float('inf')] * num_nodes
+    distances[source_node] = 0
+
+    for _ in range(num_nodes - 1):
+        for i, connections_with_weights in enumerate(nodes_input):
+            for conn, weight in connections_with_weights:
+                conn_node = int(conn.strip()) - 1
+                if weight != '+' and weight.isdigit() and distances[i] != float('inf') and distances[i] + int(weight) < distances[conn_node]:
+                    distances[conn_node] = distances[i] + int(weight)
+
+    # Check for negative cycles
+    for i, connections_with_weights in enumerate(nodes_input):
+        for conn, weight in connections_with_weights:
+            conn_node = int(conn.strip()) - 1
+            if weight != '+' and weight.isdigit() and distances[i] != float('inf') and distances[i] + int(weight) < distances[conn_node]:
+                print("Graph contains negative cycle")
+                return None
+
+    return distances
+
+
+def find_shortest_path(distances, nodes_input, source_node, current_node):
+    print("Starting find_shortest_path function")
+    shortest_path = [current_node + 1]  # Initialize the shortest path with the current node
+    
+    print("Entering while loop")
+    while current_node != source_node:
+        found = False
+        print(f"Current node: {current_node}")
+        for i, connections_with_weights in enumerate(nodes_input):
+            for conn, weight in connections_with_weights:
+                conn_node = int(conn.strip()) - 1
+                # Check if the weight is valid and if it contributes to the shortest path
+                if weight != '+' and weight.isdigit() and distances[current_node] - int(weight) == distances[conn_node]:
+                    shortest_path.append(conn_node + 1)  # Append the node to the shortest path
+                    current_node = conn_node  # Move to the connected node
+                    found = True
+                    print(f"Found valid connection to node: {conn_node}")
+                    break
+            if found:
+                break
+        
+        print("End of iteration")
+        if not found:
+            print("No valid connection found, breaking out of the loop")
+            break
+                
+    print("Exiting while loop")
+    
+    shortest_path_with_start = [source_node + 1] + shortest_path[::-1]  # Add the start node at the beginning
+    return shortest_path_with_start
+
+
+
+def menu(num_nodes, nodes_input, source_node):
     while True:
         menu_choice = int(input(
-            "\n1 | Adjacency matrix\n2 | Incidence matrix\n3 | Incidence list\n4 | View graph\n5 | BK shortest path\n0 | EXIT\n"))
+            "\n1 | View graph\n2 | BK shortest path\n3 | Ford shortest path\n0 | EXIT\n"))
         match menu_choice:
             case 1:
                 graph = create_adjacency_matrix(num_nodes, nodes_input)
-                print("\nadjacency matrix:")
-                print_adjacency_matrix(graph)
-            case 2:
-                graph = create_incidence_matrix(nodes_input)
-                print("\nincidence matrix:")
-                print_incidence_matrix(graph, num_nodes)
-            case 3:
-                graph = create_incidence_list(num_nodes, nodes_input)
-                print("\nincidence list:")
-                print_incidence_list(graph)
-            case 4:
-                graph = create_adjacency_matrix(num_nodes, nodes_input)
                 visualize_graph(graph)
-            case 5:
+            case 2:
                 result_matrix = bellman_kalaba(num_nodes, nodes_input)
                 print("\nBellman-Kalaba matrix:")
-                print_bellman_kalaba(result_matrix)
+                print_bellman_kalaba(result_matrix, nodes_input)  # Pass nodes_input here
+            case 3:
+                distances = ford_shortest_path(
+                    num_nodes, nodes_input, source_node)
+                if distances:
+                    for i, distance in enumerate(distances):
+                        print(f"Shortest distance from node {source_node + 1} to node {i + 1}: {distance}")
+                    target_node = int(input("Enter the target node: ")) - 1
+                    shortest_path = find_shortest_path(
+                        distances, nodes_input, source_node, target_node)
+                    if shortest_path:
+                        print("Shortest path:", shortest_path)
+                    else:
+                        print("No path exists from node", source_node + 1, "to node", target_node + 1)
             case 0:
                 print(
                     "\n======================\n||       Exit       ||\n======================\n")
@@ -210,7 +228,9 @@ def menu(num_nodes, nodes_input):
                 print("Invalid choice")
 
 
+
 num_nodes = int(input("node num: "))
 nodes_input = weighted_graph_connections(num_nodes)
-
-menu(num_nodes, nodes_input)
+# source_node = int(input("Enter the source node: "))
+source_node = 0
+menu(num_nodes, nodes_input, source_node)
